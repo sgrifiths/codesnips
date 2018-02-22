ee964fee28bc4429df3d7f8fa6dc1462fdeba7c3
enhancedpmd <enhancedpmd@gmail.com>
2018-02-22 14:58:20 +0000

    testing the pycharm commit function
    Signed-off-by: enhancedpmd <enhancedpmd@gmail.com>

--- random-walk.py
+++ random-walk.py
@@ -4,24 +4,24 @@ def random_walk(n):
     """return the coordinates after n random steps"""
 
 # setting up initial coordinates
- X=0
- Y=0
+    X = 0
+    Y = 0
 
- for i in range(n:
-     step=random.choice({'N','S','E','W'})
+    for i in range(n):
+        step = random.choice(['N', 'S', 'E', 'W'])
 
-     if step == 'N':
-        Y= Y+1
-     elif step == 'E':
-        x=x+1
-     elif step == 'S':
-        y=y-1
-     else:
-        x=x-1
- return(x,y)
+        if step == 'N':
+            Y = Y + 1
+        elif step == 'E':
+            X = X + 1
+        elif step == 'S':
+            Y = Y - 1
+        else:
+            X = X - 1
+    return (X, Y)
 
 
-for i in range(25):
-    walk = random_walk(100)
+for i in range(100):
+    walk = random_walk(100000)
     print(walk)
 
