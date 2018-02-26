69e951462ef15e5844a105ba6f512e621a2c151f
sgrifiths <36531972+sgrifiths@users.noreply.github.com>
2018-02-26 08:39:13 +0000

    add files vie web upload
    csv snippet  modules

---
+++ CSV_reader.py
@@ -0,0 +1,34 @@
+    import csv
+
+print(dir(csv))
+
+
+
+path = "D:\Python\CSV_convert\gsd.csv"
+
+file = open(path, newline='')
+reader = csv.reader(file
+
+
+
+#for line in file:
+#    print(line)
+
+# lines = [line for line in open(path)]
+#
+# print(lines[0])
+# print(lines[1])
+#
+#
+# print(lines[0].strip())
+# print(lines[1].strip())
+#
+#
+# print(lines[0].strip().split(','))
+# print(lines[1].strip().split(','))
+
+
+#dataset = [line.strip().split(',') for line in open(path)]
+
+print(dataset[1])
+
